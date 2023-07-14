f4daeb6eae433abb07fe889a29459a547c2ba52e
jernejak12 <jerneja.krajcar@gmail.com>
2023-07-14 09:36:03 +0200

    added more options

--- wordcloud-frontend/src/app/components/header/header.component.ts
+++ wordcloud-frontend/src/app/components/header/header.component.ts
@@ -7,12 +7,22 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
   title: string = "";
-  fontScale: number = 50;;
+  fontScale: number = 50;
+  case_UL: string = "upper";  // "upper";   // upper, lower or none
+  minWordLength: number = 3;  // 3;
+  rotation: number = 20;  // 20;
+
   @Output()
   titleChange = new EventEmitter<any>()
   onClick(){
     //the function that will send the text to api
     console.log(this.title);
-    this.titleChange.emit({text:this.title, fontScale:this.fontScale});
+    this.titleChange.emit({
+      text:this.title,
+      fontScale:this.fontScale,
+      case_UL:this.case_UL,
+      minWordLength:this.minWordLength,
+      rotation:this.rotation
+    });
   }
 }
--- wordcloud-frontend/src/app/types/get-image-params.ts
+++ wordcloud-frontend/src/app/types/get-image-params.ts
@@ -1,4 +1,7 @@
 export interface GetImageParams{
   text: string;
   fontScale: number;
+  case_UL: string;  // "upper";   // upper, lower or none
+  minWordLength: number;  // 3;
+  rotation: number;  // 20;
 }
